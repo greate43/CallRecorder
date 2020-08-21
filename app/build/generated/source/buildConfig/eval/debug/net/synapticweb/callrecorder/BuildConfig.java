714645a90530c131f2b413ce8e32709116715e21
Salman Khan <greate43.sk@gmail.com>
2020-08-21 21:55:34 +1000

    updating  json

---
+++ app/build/generated/source/buildConfig/eval/debug/net/synapticweb/callrecorder/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package net.synapticweb.callrecorder;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "net.synapticweb.callrecorder.gpcompliant.eval";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "eval";
+  public static final int VERSION_CODE = 13;
+  public static final String VERSION_NAME = "1.1.3-gpcompliant-eval";
+}
